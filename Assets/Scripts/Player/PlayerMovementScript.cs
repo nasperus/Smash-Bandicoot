d6c505cbbc733f230cb7b2769b5eff0dc2ea75ab
Giorgi Melikidze <38115148+nasperus@users.noreply.github.com>
2025-03-15 20:57:18 +0400

    New

--- Assets/Scripts/Player/PlayerMovementScript.cs
+++ Assets/Scripts/Player/PlayerMovementScript.cs
@@ -1,33 +1,52 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 
 namespace Player
 {
     public class PlayerMovementScript : MonoBehaviour
     {
+        [Header("Movement")]
         [SerializeField] private float moveSpeed;
+        
+        [Header("Rotation")]
         [SerializeField] private float rotationSpeed;
+        
+        [Header("Jump")]
         [SerializeField] private float jumpForce;
-        [SerializeField] private float groundDistance;
         [SerializeField] private float jumpUpGravity;
-        [SerializeField] private float jumpDownGravity;
-        [SerializeField] private float spinTimer;
         [SerializeField] private float airMovementSpeed;
+       
+        [Header("Spin")]
+        [SerializeField] private float spinCooldown;
+        [SerializeField] private float jumpSpinGravity;
+        
+        [Header("GroundChecks")]
+        [SerializeField] private float groundDistance;
         [SerializeField] private LayerMask groundLayer;
         [SerializeField] private GameObject groundCheck;
-
-        private float _originalMovementSpeed;
-        private bool _isGrounded;
+        
         private PlayerAnimationScript _playerAnimation;
         private Vector2 _playerInput;
         private Rigidbody _rb;
-        private const int GroundGravity = 0;
-        public static bool IsRunning { get; private set; }
-        private bool _isJumping;
-        private const int MaxJumps = 2;
-        private int _currentJumps;
+        
         private float _spinCd;
+        private float _originalMovementSpeed;
+        
+        private int _doubleJumpCounter;
+        private int _currentJumps;
+        
+        private const int MaxJumps = 2;
+        private const int X = 0;
+        private const int Z = 0;
+        private const int Y = 0;
+        private const int Zero = 0;
+        
+        private bool _canSpin;
+        private bool _isJumping;
+        private bool _isGrounded;
+        public static bool IsRunning { get; private set; }
       
         
         private void Start()
@@ -44,48 +63,54 @@ namespace Player
             DetectGround();
             JumpGravity();
             SpinCooldownTimer();
+            SpinToAirGravity();
         }
         
         private void OnMove(InputValue value) {_playerInput = value.Get<Vector2>();}
 
         private void OnFire()
         {
-            if (!(_spinCd <= 0)) return;
+            _canSpin = true;
+            if (!(_spinCd <= Zero)) return;
             _playerAnimation.SpinAnimation();
-            _spinCd = spinTimer;
+            _spinCd = spinCooldown;
         }
-
-        private  void SpinCooldownTimer()
-        {
-            if (!(_spinCd >= 0)) return;
-            _spinCd -= Time.deltaTime;   
-        }
-
+        
         private void OnJump()
         {
             if (_isGrounded)
             {
-                _currentJumps = 0;
+                _currentJumps = _doubleJumpCounter;
+                _canSpin = false;
             }
-            if (_currentJumps < MaxJumps)
+            if (_canSpin) return;
+            
+            if (_currentJumps >= MaxJumps) return;
+            _rb.linearVelocity = new Vector3(_rb.linearVelocity.x, jumpForce, _rb.linearVelocity.y);
+            _playerAnimation.JumpAnimation();
+            _currentJumps++;
+        }
+        private  void SpinCooldownTimer()
+        {
+            if (!(_spinCd >= Zero)) return;
+            _spinCd -= Time.deltaTime;   
+        }
+
+        private void SpinToAirGravity()
+        {
+            if (_canSpin && !_isGrounded)
             {
-                _rb.linearVelocity = new Vector3(_rb.linearVelocity.x, jumpForce, _rb.linearVelocity.y);
-                _playerAnimation.JumpAnimation();
-                _currentJumps++;
+                _rb.AddForce(new Vector3(X, jumpSpinGravity, Z), ForceMode.Acceleration);
             }
         }
         
         private void JumpGravity()
         {
-            if (!_isGrounded)
-            {
-                _rb.linearDamping = _rb.linearVelocity.y > 0 ? jumpUpGravity : jumpDownGravity;
-            }else
-            {
-                _rb.linearDamping = GroundGravity;
-            }
-            
+            if (_isGrounded) return;
+            if (!(_rb.linearVelocity.y > Zero)) return;
+            _rb.AddForce(new Vector3(X, jumpUpGravity, Z), ForceMode.Acceleration);
         }
+        
         
         private void DetectGround()
         {
@@ -94,7 +119,7 @@ namespace Player
         
         private void PlayerMovement()
         {
-            var movement = new Vector3(_playerInput.x, 0, _playerInput.y);
+            var movement = new Vector3(_playerInput.x, Y, _playerInput.y);
             IsRunning = movement != Vector3.zero;
             
             moveSpeed = _isGrounded ? _originalMovementSpeed : _originalMovementSpeed - airMovementSpeed;
