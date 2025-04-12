70098dda01a7f19b6016549e3dc89206c6c14599
Giorgi Melikidze <38115148+nasperus@users.noreply.github.com>
2025-04-12 23:14:09 +0400

    Update Tnt

--- Assets/Scripts/Boxes/TNTExplosiveBoxScript.cs
+++ Assets/Scripts/Boxes/TNTExplosiveBoxScript.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using Player;
+using TMPro;
 using UnityEngine;
 
 namespace Boxes
@@ -10,9 +12,12 @@ namespace Boxes
         [SerializeField] private Transform explosionStart;
         [SerializeField] private float radius;
         [SerializeField] private LayerMask explosionLayer;
+        [SerializeField] private TextMeshPro textMeshPro;
 
         private GameObject _effect;
         private Collider[] _radiusExplosions;
+        private float _countDown;
+        private bool _onCountDown;
 
         private void OnDrawGizmos()
         {
@@ -20,8 +25,26 @@ namespace Boxes
             Gizmos.DrawWireSphere(explosionStart.position, radius);
         }
 
+        private void Start()
+        {
+            textMeshPro.gameObject.SetActive(false);
+            _countDown = 3;
+        }
 
-        public void TntExplode()
+        private void Update()
+        {
+            CountDown();
+        }
+
+        private void CountDown()
+        {
+            if (!_onCountDown) return;
+            _countDown -= Time.deltaTime;
+            textMeshPro.text =Mathf.CeilToInt(_countDown).ToString();
+        }
+
+
+        private void TntExplode()
         {
             if (explosionStart == null) return;
             _radiusExplosions = Physics.OverlapSphere(explosionStart.position, radius, explosionLayer);
@@ -33,7 +56,10 @@ namespace Boxes
 
         public IEnumerator DelayExplosive()
         {
+            _onCountDown = true;
+            textMeshPro.gameObject.SetActive(true);
             yield return new WaitForSeconds(3);
+            _onCountDown = false;
             TntExplode();
         }
         public void OnSpinDamage()
